5007582ca285ac9362d2a08b7bd37638a267af8d
mdnaimur <mdnaimurr@gmail.com>
2023-12-13 07:24:45 +0600

    crud operation done

--- base/forms.py
+++ base/forms.py
@@ -18,7 +18,7 @@ class AddRecordForm(forms.ModelForm):
         fields =['first_name','last_name','email','phone','division','city','state','zipcode']
         widgets ={
             'first_name':forms.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}),
-            'last_name':forms.TextInput(attrs={"placeholder":"Lastt Name", "class":"form-control"}),
+            'last_name':forms.TextInput(attrs={"placeholder":"Last Name", "class":"form-control"}),
             'email':forms.EmailInput(attrs={"placeholder":"Email", "class":"form-control"}),
             'phone':forms.TextInput(attrs={"placeholder":"Phone ","class":"form-control"}),
             'division':forms.Select(attrs={"placeholder":"Division","class":"form-control"}),
--- base/views.py
+++ base/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render,redirect
 from .forms import AddRecordForm
 from django.contrib import messages
+from .models import Record
 # Create your views here.
 
 def home(request):
+    records = Record.objects.all()
    
-    return  render(request,'home.html')
+    return  render(request,'home.html',locals())
 
 
 def add_record(request):
@@ -17,3 +19,29 @@ def add_record(request):
             messages.success(request,"Record Added.....!")
             return redirect('home')
     return render(request,'add_record.html',locals())
+
+
+def customer_record(request,pk):
+    customer_record = Record.objects.get(id=pk)
+    return render(request,'record.html',locals())
+
+
+def update_record(request,pk):
+    current_record = Record.objects.get(id=pk)
+    form = AddRecordForm(request.POST or None,instance=current_record)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request,"Record Has Been Updated")
+        return redirect('home')
+    return render(request,'update_record.html',locals())
+
+
+def delete_record(request,pk):
+    record = Record.objects.get(id=pk)
+    if(record == None ):
+        messages.info(request,"Record Not found!!")
+        return render(request,'update_record.html')
+    record.delete()
+    messages.success(request,"Record Deleted Successfully!!")
+    return redirect('home')
