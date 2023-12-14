c2ca4b74509c95e662433f8e39146268df74716b
mdnaimur <mdnaimurr@gmail.com>
2023-12-14 10:03:21 +0600

    authenticatin done

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Customer Relations Management 
+
+
+
+This example demonstrates a simple Django web application with CRUD functionality and user authentication.
+
+## Prerequisites
+
+
+
+## Setup
+
+1. Clone the repository:
+
+   ```bash
+   git clone hhttps://github.com/mdnaimur/crm.git
+   cd crm
+
+
+
+
--- base/views.py
+++ base/views.py
@@ -1,13 +1,66 @@
-from django.shortcuts import render,redirect
-from .forms import AddRecordForm
 from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.shortcuts import render
+
+from .forms import AddRecordForm
+from .forms import CustomUserCreationForm
 from .models import Record
+
 # Create your views here.
 
 def home(request):
     records = Record.objects.all()
    
     return  render(request,'home.html',locals())
+
+#loging
+def login_user(request):
+
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        #Authenticate
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,'You have been logged in')
+            return redirect('home')
+        else:
+            messages.warning(request,'Username or Password does not match')
+            return render(request,'login.html')
+            
+    return render(request,'login.html')
+
+#Logout User
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You Have Been Logged Out...")
+    return redirect('home')   
+
+#Registraion
+def register_user(request):
+    if request.method=='POST':
+       form = CustomUserCreationForm(request.POST)
+       if form.is_valid():
+           form.save()
+
+           #Authenticate and login
+           username = form.cleaned_data['username']
+           password = form.cleaned_data['password1']
+           user = authenticate(request,username=username,password=password)
+           login(request,user)
+           messages.success(request,'You have successfully restringraion. Welcome')
+           return redirect('home')
+    
+    else:
+        form = CustomUserCreationForm()
+        return render(request,'register.html',locals())
+
+    return render(request,'register.html',locals())
 
 
 def add_record(request):
